249dd06ec9d6f432f555e0afd647cccf21ebf9be
yosnak13 <yosnak13@gmail.com>
2021-03-15 07:16:56 +0900

    create form_with for sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      # 保存の成功をここで扱う。
     else
-      render 'mew'
+      render 'new'
     end
   end
 
