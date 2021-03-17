e1cd16f5091ed6dcb94b6f16686b4ef2ad8e4a8c
yosnak13 <yosnak13@gmail.com>
2021-03-17 23:13:25 +0900

    session削除でログインうまく行った

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
       log_in user #sessions_helperでloginメソッドを定義している
       redirect_to user
     else
