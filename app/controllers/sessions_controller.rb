bc9ab46b0014311f6308c26b446f2afcc9e9d8ce
yosnak13 <yosnak13@gmail.com>
2021-03-18 07:31:52 +0900

    ログインユーザーの保持

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       log_in user #sessions_helperでloginメソッドを定義している
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
