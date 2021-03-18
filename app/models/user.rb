b5c29bb59d779640a8c151fec786a54d71613f8f
yosnak13 <yosnak13@gmail.com>
2021-03-19 07:17:15 +0900

    fix syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,18 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  class << self
+   class << self
     # 渡された文字列のハッシュ値を返す
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
-  end
 
-  # ランダムなトークンを返す
-  def new_token
-    SecureRandom.urlsafe_base64
+    # ランダムなトークンを返す
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   # 永続セッションのためにユーザーをデータベースに記憶する
