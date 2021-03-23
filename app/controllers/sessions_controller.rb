9656aff180b1afff165181430ce6b22c8d31c232
yosnak13 <yosnak13@gmail.com>
2021-03-24 07:46:37 +0900

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       log_in user #sessions_helperでloginメソッドを定義している
       params[:remember_me] == '1' ? remember(user) : forget(user)
+      # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
