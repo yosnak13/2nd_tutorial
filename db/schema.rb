d41ddee0c2259a2034547d6202eed4e207dd46dd
yosnak13 <yosnak13@gmail.com>
2021-03-26 07:48:47 +0900

    adminをusersテーブルに 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210317221205) do
+ActiveRecord::Schema.define(version: 20210325224750) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20210317221205) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
