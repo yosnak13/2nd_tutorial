496c03bab8aff4f5c01f5f9bfbeb9143c668f63e
yosnak13 <yosnak13@gmail.com>
2021-03-27 07:46:25 +0900

    create User mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   before_save   :downcase_email
   before_create :create_activation_digest
 
-  attr_accessor :remember_token
-  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
