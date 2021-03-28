ed599f5b718b1f06d6e0758d8b7d94f0491c7e2e
yosnak13 <yosnak13@gmail.com>
2021-03-28 16:26:31 +0900

    following" 関連のメソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -86,6 +86,21 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
   private
   # メールアドレスをすべて小文字にする
     def downcase_email
