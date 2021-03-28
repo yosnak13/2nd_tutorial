87087620ab450fa903f81fb95d4cd69c4fc4f3fd
yosnak13 <yosnak13@gmail.com>
2021-03-28 15:12:57 +0900

    homeアクションにフィードのインスタンス変数を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,6 +78,10 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
   # メールアドレスをすべて小文字にする
     def downcase_email
